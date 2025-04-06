d06c283e440dd95df96c307ed2e49d97177860ec
Fritz-nvm <fritzimpah@gmail.com>
2025-04-06 20:38:40 +0100

    added the models and the logout view

--- custom_models/myapp/views.py
+++ custom_models/myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
+
+
+class LoginView(LoginView):
+    template_name = 'login.html'
+    
+class LogoutView(LogoutView):
+    template_name = 'logout.html'
+
